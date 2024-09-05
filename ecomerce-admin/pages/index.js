5ac75b64d9dc53ad31c6ad061c5b727ae6f78573
kedabudre <abdurehimk77@gmail.com>
2024-09-06 00:35:57 +0200

    initial commit

---
+++ ecomerce-admin/pages/index.js
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen bg-blue-500">
+      <div className="text-center f-width">
+          <button
+            className="bg-green-500 text-white px-4 py-2 rounded-lg"
+          >
+            Sign In with Google
+          </button>
+      </div>
+    </div>
+  );
+}
