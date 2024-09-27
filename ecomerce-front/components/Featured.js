1ff660cab1437c8b27c4e60a76f9b7d9ca2287bc
kedabudre <abdurehimk77@gmail.com>
2024-09-27 17:21:55 +0200

    update the featured component to fetch data from db and display to front

--- ecomerce-front/components/Featured.js
+++ ecomerce-front/components/Featured.js
@@ -2,6 +2,7 @@ import Button from "./Button";
 import Center from "./Center";
 import styled from "styled-components";
 import CartIcon from "./icons/CartIcon";
+import ButtonLink from "./ButtonLink";
 
 const Bg = styled.div`
   background-color: #222;
@@ -10,19 +11,21 @@ const Bg = styled.div`
 `;
 
 const Title = styled.h1`
+  font-size: 2.5rem;
   margin: 0;
   font-weight: normal;
 `
 
 const Desc = styled.p`
   color: #aaa;
-  font-size: .8rem;
+  font-size: .9rem;
+  line-height: 1.5;
 `;
 
 const ColumnWrapper = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 40px;
+  grid-template-columns: .9fr 1.1fr;
+  gap: 50px;
   img {
     max-width: 100%;
   }
@@ -39,17 +42,18 @@ const Column = styled.div`
   align-items: center;
 `;
 
-export default function Featured() {
+export default function Featured({product}) {
+  console.log(product)
   return (
     <Bg>
       <Center>
         <ColumnWrapper>
           <Column>
             <div>
-              <Title>Pro anywhere</Title>
-              <Desc>Lorem ipsum dolor sit amet, consectetur adipiscing elit  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Desc>
+              <Title>{product.title}</Title>
+              <Desc>{product.description}</Desc>
               <ButtonWrapper>
-                <Button white outline>Read more</Button>
+                <ButtonLink white outline href={"/products/" + product._id}>Read more</ButtonLink>
                 <Button primary>
                   <CartIcon />
                   Add to cart
@@ -59,7 +63,7 @@ export default function Featured() {
           </Column>
           <Column>
             <div>
-              <img src="https://i.imgur.com/tKN29Pn.png" alt="Pro anywhere" />
+              <img src={"https://dawid-next-ecommerce.s3.amazonaws.com/1679151719649.png"} alt="Pro anywhere" />
             </div>
           </Column>
         </ColumnWrapper>
--- ecomerce-front/pages/index.js
+++ ecomerce-front/pages/index.js
@@ -1,11 +1,23 @@
 import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
-export default function Home() {
+
+export default function Home({ product }) {
   return (
     <div>
       <Header />
-      <Featured />
+      <Featured product={product} />
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = "66ef40e5b00953c074930388"
+  await mongooseConnect()
+  const product = await Product.findById(featuredProductId)
+   return {
+    props: { product: JSON.parse(JSON.stringify(product)) },
+  }
 }
