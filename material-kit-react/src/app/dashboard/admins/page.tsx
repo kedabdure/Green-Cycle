d6d4fbfe819962e0cd4f953955f62a9f13ce60d7
kedabudre <abdurehimk77@gmail.com>
2024-10-21 20:36:06 +0300

    update admin page to support table to pdf

--- material-kit-react/src/app/dashboard/admins/page.tsx
+++ material-kit-react/src/app/dashboard/admins/page.tsx
@@ -1,21 +1,27 @@
 "use client";
 
 import * as React from 'react';
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import { Download as DownloadIcon } from '@phosphor-icons/react/dist/ssr/Download';
 import { Plus as PlusIcon } from '@phosphor-icons/react/dist/ssr/Plus';
-import { Upload as UploadIcon } from '@phosphor-icons/react/dist/ssr/Upload';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
 
 import { AdminsTable } from '@/components/dashboard/admin/admins-table';
 import { ScaleSpinner } from '@/components/loader/spinner';
 import { AdminsFilters } from '@/components/dashboard/admin/admins-filters';
 import Link from 'next/link';
 import AdminProps from '@/types/admin';
+
+dayjs.extend(relativeTime);
 
 const fetchCustomers = async () => {
   const { data } = await axios.get('/api/admins');
@@ -35,6 +41,33 @@ export default function Page(): React.JSX.Element {
     admin.email.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  const exportToPDF = () => {
+    const doc = new jsPDF();
+
+    // DEFINE TABLE COLUMNS+
+    const tableColumn = ["Name", "Email", "Phone", "Status", "Registered At"];
+    const tableRows: string[][] = [];
+
+    admins.forEach((admin: AdminProps) => {
+      const orderData = [
+        admin.name,
+        admin.email,
+        admin.phone,
+        admin.status,
+        dayjs(admin.createdAt).format('MMM D, YYYY'),
+      ];
+      tableRows.push(orderData);
+    });
+
+    // ADD TABLE to the PDF
+    autoTable(doc, {
+      head: [tableColumn],
+      body: tableRows,
+    });
+
+    doc.save('admins.pdf');
+  };
+
 
   return (
     <Suspense fallback={<ScaleSpinner />}>
@@ -43,10 +76,7 @@ export default function Page(): React.JSX.Element {
           <Stack spacing={1} sx={{ flex: '1 1 auto' }}>
             <Typography variant="h4">Customers</Typography>
             <Stack direction="row" spacing={1} sx={{ alignItems: 'center' }}>
-              <Button color="inherit" startIcon={<UploadIcon fontSize="var(--icon-fontSize-md)" />}>
-                Import
-              </Button>
-              <Button color="inherit" startIcon={<DownloadIcon fontSize="var(--icon-fontSize-md)" />}>
+              <Button color="inherit" onClick={exportToPDF} startIcon={<DownloadIcon fontSize="var(--icon-fontSize-md)" />}>
                 Export
               </Button>
             </Stack>
@@ -62,8 +92,8 @@ export default function Page(): React.JSX.Element {
             </Link>
           </div>
         </Stack>
-        <AdminsFilters onSearch={setSearchQuery}/>
-        <AdminsTable rows={filteredAdmins} isLoading={isLoading}/>
+        <AdminsFilters onSearch={setSearchQuery} />
+        <AdminsTable rows={filteredAdmins} isLoading={isLoading} />
       </Stack>
     </Suspense>
   );
