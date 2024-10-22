07dc4db9d4e2afaff25826e20ed474c36a6d69db
kedabudre <abdurehimk77@gmail.com>
2024-10-22 13:06:49 +0300

    update some files

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -6,7 +6,6 @@ import type { Viewport } from 'next';
 import '@/styles/global.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { UserProvider } from '@/contexts/user-context';
 import { LocalizationProvider } from '@/components/core/localization-provider';
 import { ThemeProvider } from '@/components/core/theme-provider/theme-provider';
@@ -32,7 +31,6 @@ export default function Layout({ children, session }: LayoutProps): React.JSX.El
                 <ThemeProvider>{children}</ThemeProvider>
               </UserProvider>
             </SessionProvider>
-            <ReactQueryDevtools />
           </QueryClientProvider>
         </LocalizationProvider>
       </body>
