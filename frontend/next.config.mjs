49315f40f351ece6610efce2ca45ebff2a6da977
kedabudre <abdurehimk77@gmail.com>
2024-10-23 16:30:19 +0300

    update eslint c config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,23 +1,4 @@
-import { hostname } from 'os';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '*.googleusercontent.com'
-      },
-      {
-        protocol: 'https',
-        hostname: 'ik.imagekit.io',
-      },
-    ]
-  }
-};
+const nextConfig = {};
 
 export default nextConfig;
---
+++ frontend/src/app/page.js
@@ -0,0 +1,8 @@
+import React from "react"
+export default function Home() {
+  return  (
+    <div className="w-[100%] h-[100vh] flex items-center justify-center">
+      First deployment test with Vercel
+    </div>
+  )
+}
