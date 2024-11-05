3102ecd35be8a514b37f296b0661bc260810db4c
kedabudre <abdurehimk77@gmail.com>
2024-11-05 13:31:59 +0300

    create a mongos db model for buying object from the user

---
+++ frontend/models/Buy.js
@@ -0,0 +1,19 @@
+import { model, models, Schema } from 'mongoose';
+
+const BuySchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: tru},
+  phone: { type: String, required: true },
+  country: { type: String, required: true },
+  city: { type: String, required: true },
+  subCity: { type: String, required: true },
+  streetAddress: { type: String, required: true },
+  estimatedPrice: { type: Number, required: true },
+  additionalInfo: { type: String },
+  images: { type: [String], required: true },
+}, {
+  timestamps: true
+});
+
+export const Buy = models.Buy || model('Buy', BuySchema);
